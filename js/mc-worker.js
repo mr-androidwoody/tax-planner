11949a56acfb2145bd462f65e54cf634d0f28352
mr-androidwoody <android.woody@gmail.com>
2026-04-23 21:53:11 +0100

    Add files via upload

--- js/mc-worker.js
+++ js/mc-worker.js
@@ -417,7 +417,7 @@ function runPath(inputs, equityVol, inflationVol, stressMode, stressParams) {
       // growth unchanged
     } else if (stressMode === 'lostDecade' && stressParams) {
       const winStart = stressParams.lostDecadeStart;
-      if (yi >= winStart && yi < winStart + 10) growthMean = -0.005;
+      if (yi >= winStart && yi < winStart + 10) growthMean = -0.015;
       // inflationVol unchanged
     }
 
